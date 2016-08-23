794e24e7cbb0d22dba3df16b5bb7ecceb590de54
David Ervideira <david.ervideira@inw-tech.com>
2016-08-23 10:31:44 +0100

    add unit test for forecastio class

--- tests/forecastiopy_test.py
+++ tests/forecastiopy_test.py
@@ -1,9 +1,28 @@
 import unittest
+import sys
+sys.path.insert(0, '../forecastiopy')
 
+import ForecastIO
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class ForecastIOTest(unittest.TestCase):
+
+    def setUp(self):
+        self.fio = ForecastIO.ForecastIO('0' * 32)
+        with open('lisbon_forecast.json') as f:
+            json = f.read()
+            self.fio.get_forecast_from_string(json)
+
+    def test_latitude(self):
+        self.assertEqual(self.fio.latitude, 38.7252993)
+
+    def test_longitude(self):
+        self.assertEqual(self.fio.longitude, -9.1500364)
+
+    def test_timezone(self):
+        self.assertEqual(self.fio.timezone, 'Europe/Lisbon')
+
+    def test_offset(self):
+        self.assertEqual(self.fio.offset, 1)
 
 
 if __name__ == '__main__':
