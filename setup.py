7e330519764b2007dabfcac5949c93fce4f18b82
David Ervideira <david.ervideira@inw-tech.com>
2016-10-11 17:17:47 +0100

    update setup.py to new version

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 
 
 setup(name='forecastiopy',
-      version='0.2',
-      description='A wrapper to access weather data provided by forecast.io',
+      version='0.21',
+      description='A wrapper to access weather data provided by darksky.net',
       url='http://github.com/dvdme/forecastiopy',
       download_url='http://github.com/dvdme/forecastiopy/tarball/0.2',
       author='David Ervideira',
       author_email='david.dme@gmail.com',
       license='Eclipse Public License',
       packages=['forecastiopy'],
-      keywords=['weather','forecastio','rest'],
+      keywords=['weather','forecastio', 'forecast.io','rest', 'darksky','darksky.net'],
       install_requires=[
           'requests',
       ],
